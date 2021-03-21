a3cc5f1fe50fd0972866881a4c9dba4af2801e43
hamza-mughees <hamzamughees@gmail.com>
2021-03-21 19:27:00 +0000

    Ability to write processed data in csv file

--- load_and_process_data.py
+++ load_and_process_data.py
@@ -5,7 +5,7 @@ import time
 from PIL import Image
 
 STORAGEACCOUNTNAME= 'covid19detecti4594663496'
-STORAGEACCOUNTKEY=''
+STORAGEACCOUNTKEY='1xRicfWfUa9rgqyMyiASbzIS+E+kNPKKIropOG/UT63z2/E9+oqrH3Htv/0PjYj6EkH+vSC7PaazBJNjW03lJA=='
 CONTAINERNAME= 'sourcedata'
 CSV_LOCAL_FILE_NAME= 'local_data_file.csv'
 CSV_FILE_BLOB_NAME= 'Covid-19-1/data_file.csv'
@@ -19,34 +19,42 @@ img_blob = BlockBlobService(account_name=STORAGEACCOUNTNAME, account_key=STORAGE
 # Original csv dataframe
 csv_df = pd.read_csv(CSV_LOCAL_FILE_NAME)
 
-def pixels_from_path(file_path, new_size):   #returns matrix of pixel RGB values
+def pixels_from_path(file_path, new_size):
     img = Image.open(file_path)
     img = img.convert('L')
     img = img.resize(new_size)
     np_img = np.array(img)
     return np_img
 
-for i in range(335, csv_df.shape[0]):
+processed_data_file_name = 'processed_data.csv'
+
+resize_to = 224
+data = []
+
+for i in range(csv_df.shape[0]):
     curr_img_name = csv_df['filename'][i]
     img_blob.get_blob_to_path(CONTAINERNAME, f'Covid-19-1/images/{curr_img_name}', 'current_image.jpeg')
 
-    np_img = pixels_from_path(file_path='current_image.jpeg', new_size=(224, 224))
+    np_img = pixels_from_path(file_path='current_image.jpeg', new_size=(resize_to, resize_to))
+    np_img = np_img.reshape(1, resize_to*resize_to)
 
-    np_img = np_img.reshape(1, 50176)
-    row = np.append(np_img[0], [1], axis=0)
+    diagnosis = 1 if csv_df['diagnosis'][i] == 'positive' else 0
+    row = np.append(np_img[0], [diagnosis], axis=0)
 
-    if i == 0:
-        data = np.array([row])
-    else:
-        data = np.append(data, np.array([row]), axis=0)
+    data.append(row)
 
-df = pd.DataFrame(data)
+    print(f'Successfully processed \'{curr_img_name}\'')
+    
+    if i == 20:
+        break
 
-print(df.head())
+df = pd.DataFrame(np.array(data))
 
+#print(df)
 
-
-
+data_file = open(f'./{processed_data_file_name}', 'w')
+data_file.write(df.to_csv(index=False))
+data_file.close()
 
 
 
