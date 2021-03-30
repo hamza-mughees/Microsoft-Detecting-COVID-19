bb0034693d9f40678929943ad9f74a79d388ca30
sarahdolan1 <sarahdolan1969@hotmail.com>
2021-03-30 10:22:07 +0000

    optimizing model

--- prototype.py
+++ prototype.py
@@ -6,23 +6,24 @@ from keras.preprocessing import image
 
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=3, activation='relu', input_shape=(224,224,3)))
-model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(Conv2D(32, kernel_size=3, activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(224,224,3)))
+model.add(MaxPooling2D(pool_size=2))
+model.add(Conv2D(64, kernel_size=3, activation='relu', kernel_initializer='he_uniform', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, kernel_size=3, activation='relu'))
+model.add(Conv2D(64, kernel_size=3, activation='relu', kernel_initializer='he_uniform', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(128, kernel_size=3, activation='relu'))
+model.add(Conv2D(128, kernel_size=3, activation='relu',kernel_initializer='he_uniform', padding='same'))
 model.add(MaxPooling2D(pool_size=2))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(1, activation='softmax'))
 
 model.compile(loss=keras.losses.binary_crossentropy, optimizer='adam', metrics=['accuracy'])
 
@@ -54,7 +55,7 @@ validation_generator = test_datagen.flow_from_directory(
 hist = model.fit_generator(
     train_generator,
     steps_per_epoch = 8,
-    epochs = 10,
+    epochs = 1,
     validation_data = validation_generator,
     validation_steps = 2,
 )
