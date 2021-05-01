2a2c30c60e838bcf82757217305bbb5fd9169099
jason mullen <mullenj7@tcd.ie>
2021-05-01 12:53:33 +0000

    working api call, deleted unecessary files

--- inference_config.py
+++
@@ -1,6 +0,0 @@
-from azureml.core.model import InferenceConfig
-
-classifier_inference_config = InferenceConfig(runtime= "python",
-                                              source_directory = '',
-                                              entry_script="scoring_script.py",
-                                              conda_file="env.yml")
